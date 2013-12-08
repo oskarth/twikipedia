21310cffa3c7c5462fbf8f65f34579648413d5e8
Oskar Thoren <o.thoren@gmail.com>
2013-12-09 00:34:05 +0100

    Add file text db

--- src/twikipedia/views.clj
+++ src/twikipedia/views.clj
@@ -1,27 +1,18 @@
 (ns twikipedia.views
   (:use [hiccup core page element]))
 
-(def lorem
-  "Lorem ipsum dolor sit amet, consectetur adipiscing
-  elit. Morbi ut lorem nec enim aliquet placerat id tincidunt
-  massa. Integer sit amet nibh ac neque vestibulum rutrum. Aliquam
-  venenatis tincidunt lectus nec hendrerit. Aliquam vestibulum volutpat
-  commodo. Praesent volutpat, diam vitae imperdiet semper, dui nibh
-  sodales ante, quis sagittis massa felis a eros. Vivamus eu elit vitae
-  felis pharetra facilisis. Sed dolor libero, dapibus a turpis at,
-  mollis lacinia odio. Aenean bibendum felis ipsum, quis ullamcorper
-  lacus tempus ut. Quisque lectus sapien, egestas vitae enim a, cursus
-  pharetra dui.
+;; (def db (atom {}))
 
-Etiam porta, sapien sit amet feugiat scelerisque, nunc sapien mattis
-lorem, id eleifend nunc libero eget neque. Sed quis vulputate
-magna. Duis eu lacinia ligula. Etiam condimentum neque ut sapien
-eleifend commodo. Pellentesque tincidunt ante sem, nec tempor quam
-tempus at. Aliquam et varius mauris. Nulla tempor eros vel eleifend
-congue. Cras non eleifend libero. Proin pellentesque commodo lectus
-amet.")
+(defn save-data []
+  (spit "data.db" (prn-str @db)))
 
-(def db (atom {:foo "lix" :bar "lux"}))
+(defn load-data []
+  (reset! db (read-string (slurp "data.db"))))
+
+(save-data)
+(load-data)
+
+(def lorem (:text (:cell-cycle @db)))
 
 (def cell-cycle-img
   [:img
