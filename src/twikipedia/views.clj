2771c213ae331eda8e8c61513d87fa62ff1521a3
Oskar Thoren <o.thoren@gmail.com>
2013-12-09 01:06:18 +0100

    Add front page with list.

--- src/twikipedia/views.clj
+++ src/twikipedia/views.clj
@@ -1,16 +1,17 @@
 (ns twikipedia.views
   (:use [hiccup core page element]))
 
-;; (def db (atom {}))
+(def db (atom {}))
+(defn load-data []
+  (reset! db (read-string (slurp "data.db"))))
+
+(load-data)
 
 (defn save-data []
   (spit "data.db" (prn-str @db)))
 
-(defn load-data []
-  (reset! db (read-string (slurp "data.db"))))
-
-(save-data)
-(load-data)
+;;(save-data)
+;;(load-data)
 
 (def lorem (:text (:cell-cycle @db)))
 
@@ -24,40 +25,43 @@
     [:title "Twikipedia - soul of wit?"]
     (include-css "/css/style.css")
     [:body
-     [:div#wrapper body]]]))
+     [:div#topbar
+     [:h1 (link-to "/" "TWIKIPEDIA")]]
+     [:div#wrap body
+      [:hr]
+      [:div#underbar "short and sweet, once again they meet"]]
+     ]]))
 
 (defn index-page []
   (template
-   (html5
-   [:div [:h1 "The Cell Cycle"]]
-    cell-cycle-img
-    [:section ;; this is for the editable
-     [:div {:contenteditable "true"}
-      lorem]]
-    [:div {:align "right"}
-     ;;[:button {:name "btn"} "Edit"]
-    ;;[:button {:name "save"} "Save"]
-     ]))) ;; add action here
+   (html
+    "Not wikipedia, nor a dictionary. We like brevity, examples and images;
+dislike opaqueness and sticklers. Pour a glass and have a browse!"
+    [:h2
+     [:ol
+      [:li (link-to "/wiki/cell-cycle" "The Cell Cycle")]
+      [:li (link-to "/wiki/apoptosis" "Apoptosis")]
+      [:li (link-to "/wiki/bistability" "Bistabilty")]
+      [:li (link-to "/wiki/bifurcation" "Bifurcation")]
+      [:li (link-to "/wiki/how-to-make-scrambled-eggs" "How To Make Scrambled Eggs")]]])))
 
 (defn page-in-db? [page]
   (not (nil? ((keyword page) @db))))
 
 (defn wiki-html [page]
-  (html5
-   [:head (include-css "/css/style.css")]
-   [:body
-    [:div#topbar
-     [:h1 (link-to "/" "TWIKIPEDIA")]]
-    [:div#wrap {:margin-top "-1em"}
-     [:h2 "The Cell Cycle"]
-     [:hr]
-     [:img
-      {:src "/img/cell-cycle.png"
-       :width "300" :height "300"
-       :align "right"}]
-     lorem
-     [:hr]
-     [:div#underbar "short and sweet, once again they meet"]]])) ;; toggle contenteditable
+  (template
+   (html5
+    [:body
+     [:div#wrap {:margin-top "-1em"}
+      [:h2 "The Cell Cycle"]
+      [:hr]
+      [:img
+       ;; where does width height align info belong?
+       {:src "/img/cell-cycle.png"
+        :width "300" :height "300"
+        :align "right"}]
+      lorem]])))
+;; toggle contenteditable
 ;; "brevis esse laboro, obscurus fio"
 ;; one of thirty two
 ;; a book has barely any state at all. what page you are at, and that is it.
@@ -67,4 +71,6 @@
 (defn wiki-page [page]
   (if (page-in-db? page)
     (wiki-html (str ((keyword page) @db)))
-    "New page."))
+    (str "New page. " page)))
+
+(page-in-db? "cell-cycle")
